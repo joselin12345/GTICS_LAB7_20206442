c680820e84664187e6414b47753cf1a5cd41c3a9
joselin <a20206442@pucp.edu.pe>
2023-09-28 20:37:08 -0500

    proyecto base

---
+++ lab7_20206442/src/main/java/com/example/lab7_20206442/Lab720206442Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20206442;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720206442Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab720206442Application.class, args);
+    }
+
+}
